72b41146a0f5f7bf3a2dae9133742c751ab5a08a
Chris Skinner <c.skinner@blueyonder.co.uk>
2017-04-27 01:23:37 +0100

    Adjust indent from new eslint file | add browser reload to dev watch task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,6 @@ import cleanCSS from 'gulp-clean-css';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
 import maps from 'gulp-sourcemaps';
-import babel from 'gulp-babel';
 import del from 'del';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
@@ -19,11 +18,11 @@ const rootDir = '';
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-    return gulp.src(rootDir + 'less/main.less')
-        .pipe(maps.init())
-        .pipe(less())
-        .pipe(maps.write('./'))
-        .pipe(gulp.dest(rootDir + 'css'));
+   return gulp.src(rootDir + 'less/main.less')
+     .pipe(maps.init())
+     .pipe(less())
+     .pipe(maps.write('./'))
+     .pipe(gulp.dest(rootDir + 'css'));
 });
 
 // Minify compiled CSS
@@ -36,7 +35,7 @@ gulp.task('minify-css', ['less'], function() {
       .pipe(gulp.dest(rootDir + 'css'))
       .pipe(browserSync.reload({
          stream: true
-   }));
+      }));
 });
 
 // Concat Minify Browserify SourceMap JS
@@ -51,55 +50,55 @@ gulp.task('minify-js', function() {
       .pipe(maps.write('./')) // write sourcemap
       .pipe(gulp.dest(rootDir + 'js'))
       .pipe(browserSync.reload({
-          stream: true // Reload Browser
+         stream: true // Reload Browser
       }));
 });
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-    return gulp.src([
-        'node_modules/bootstrap/**',
-        ])
-    .pipe(gulp.dest('vendor/bootstrap')),
+   return gulp.src([
+      'node_modules/bootstrap/**',
+   ])
+   .pipe(gulp.dest('vendor/bootstrap')),
 
-    gulp.src([
-        'node_modules/font-awesome/**',
-        ])
-    .pipe(gulp.dest('vendor/font-awesome'));
+   gulp.src([
+      'node_modules/font-awesome/**',
+   ])
+   .pipe(gulp.dest('vendor/font-awesome'));
 });
 
 // Copy fonts over
 gulp.task('fonts', ['copy'], function(){
-    return gulp.src([
-        'vendor/font-awesome/fonts/**'
-        ])
-    .pipe(gulp.dest(rootDir + 'fonts'));
+   return gulp.src([
+      'vendor/font-awesome/fonts/**'
+   ])
+   .pipe(gulp.dest(rootDir + 'fonts'));
 });
 
 // Clean up task
 gulp.task('clean', function(){
-    del(['dist']);
+   del(['dist']);
 });
 
 // Create dist folder
 gulp.task('dist', ['clean'], function() {
-    return gulp.src([
-        'css/*.min.*',
-        'js/*.min.js*',
-        'fonts/*',
-        'index.html',
-        'img/**'],
-        { base: './' })
+   return gulp.src([
+      'css/*.min.*',
+      'js/*.min.js*',
+      'fonts/*',
+      'index.html',
+      'img/**'],
+     { base: './' })
     .pipe(gulp.dest(rootDir + 'dist'));
 });
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: rootDir
-        },
-     });
+   browserSync.init({
+      server: {
+         baseDir: rootDir
+      },
+   });
 });
 
 // NOTE: First run everything to get setup
@@ -108,10 +107,10 @@ gulp.task('setup', ['fonts']);
 // Dev task with browserSync and watch
 gulp.task('dev', ['browserSync', 'minify-css', 'minify-js'], function() {
     // Watch file changes
-    gulp.watch(['less/*.less'], ['minify-css']);
-    gulp.watch(['js/*.js', '!js/*.min.js'], ['minify-js'] );
-    // Reloads the browser on file change
-    gulp.watch('*.html', browserSync.reload);
+   gulp.watch(['less/*.less'], ['minify-css'], browserSync.reload);
+   gulp.watch(['js/*.js', '!js/*.min.js'], ['minify-js'], browserSync.reload );
+   // Reloads the browser on file change
+   gulp.watch('*.html', browserSync.reload);
 });
 
 // Default build task with dist creation
